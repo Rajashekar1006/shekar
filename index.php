910ff1a300a04544b8f0046b1c31696a3827212f
rajashekar1006 <rajashekaraf1006@gmail.com>
2015-11-30 10:03:16 +0530

    add

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Calm breeze login screen</title>
+    
+    
+    
+    
+        <link rel="stylesheet" href="css/style.css">
+
+    
+    
+    
+  </head>
+
+  <body>
+
+    <div class="wrapper">
+	<div class="container">
+		<h1>Welcome</h1>
+		
+		<form class="form" method="post" action="logincheck.php" enctype="multipart/form-data" >
+			<input type="text"  name="stud_name" placeholder="name">
+			<input type="text" name="stud_email" placeholder="email">
+			<button type="submit" name="submit" >Login</button>
+		</form>
+	</div>
+	
+	<ul class="bg-bubbles">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+</div>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+        <script src="js/index.js"></script>
+
+    
+    
+    
+  </body>
+</html>
